1d0ac5b8dd4a519633abe5a1b0444a6853abde23
Murat <cankaya.murat23@gmail.com>
2020-12-14 07:05:24 +0100

    header added

--- BCOSFIRE.py
+++ BCOSFIRE.py
@@ -2,6 +2,24 @@ import numpy as np
 import cosfire as c
 
 
+""" A Python implementation of the B-COSFIRE algorithm.
+
+B-COSFIRE aims to detect elongated patterns in images such as blood vessels in retinal images. 
+It uses the existing COSFIRE (Combination Of Shifted Filter Responses) algorithm to get orientation selectivity. 
+More specifically, the algorithm computes the weighted geometric mean of the output of a pool of Difference-of-Gaussians filters to detect vessel-like patterns in images.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the BSD General Public License as published by The COSFIRE Consolidation Project, version 0.0.1.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the BSD General Public License for more details.
+You should have received a copy of the BSD General Public License along with
+this program. If not, see https://github.com/Brains-for-hire/bcosfire_python/blob/main/LICENSE.
+"""
+
+
+
 def BCOSFIRE(img_rgb, mask=[]):
 	## Model configuration
 
@@ -54,8 +72,20 @@ if __name__ == '__main__':
 	mask = np.ones(shape=img_rgb.shape[:-1])
 
 	resp,segresp = BCOSFIRE(img_rgb,mask)
-	resp = resp[20:-20,20:-20]
+	resp = resp[20:-20,20:-20] # Remove padding
 	p_vessel = resp/np.amax(resp)
 	# plt.imsave('./figures/sample_0_out.png',p_vessel,cmap='gray')
 	plt.imshow(p_vessel,cmap='gray')
 	plt.show()
+
+
+
+__author__ = "Murat Cankaya"
+__contact__ = "info@brainsforhire.eu"
+__copyright__ = "Copyright 2020, Brains for hire UG"
+__credits__ = ["Nicola Strisciuglio"]
+__date__ = "2020/12/14"
+__deprecated__ = False
+__email__ =  "info@"
+__license__ = "BSD3"
+__version__ = "0.0.1"
